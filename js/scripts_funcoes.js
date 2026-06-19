cc5a388c7e11297c2c65f3e5a31ac72907c46a56
marcella <marcellamatos2428@gmail.com>
2026-06-19 14:49:14 -0300

    arrow function, settimeout,setinterval

--- js/scripts_funcoes.js
+++ js/scripts_funcoes.js
@@ -10,15 +10,72 @@ function mensagemlinda(txt){
 mensagemlinda('Brasil vai dar um pau no brasil')
 
 function somanumeros(num1, num2){
-    return num1 + num2
+    reposta = num1 + num2
 } 
 
 console.log('As somas dos numeros', somanumeros(10,58))
 
 const multiplicacao = function(num1, num2){
-    return num1 * num2
+    reposta = num1 * num2
 }
 
 console.log(multiplicacao(64, 5))
 
 console.log('-------> Arrow function <---------')
+
+const msgarrow = () => {
+    console.log('outra mensagem na arrow function')
+}
+
+msgarrow()
+
+const divisaonum = (valor1, valor2) =>{
+    
+    if ((valor1 > 0) && (valor2 > 0)){
+        reposta  = valor1 / valor2
+        }else{
+            reposta = 'nao é possivel diviao por 0'
+        }
+
+        return reposta
+    }
+
+    console.log(divisaonum(10,8000))
+
+    console.log(Math.random())
+
+    console.log('o numero sorteado =====>',parseInt(Math.random() * 100))
+
+setTimeout(()=>{
+    console.log('execetuou deppooiis de 5 s')
+    msgarrow()
+}, 5000)
+
+//let cont = 0 
+//setInterval(()=>{
+   // cont++
+    //console.log(cont, "repetiçao da setInterval")
+
+    //if(cont == 5){
+       // clearInterval(intervalo)
+   // }
+//},// 3000)
+
+let contvolta = 0, numsorteado = 0
+
+const sorteo = setInterval(()=>{
+    contvolta++
+
+    numsorteado = parseInt(Math.random() * 60)
+
+    console.log(`${contvolta} numero sorteado ${numsorteado} `)
+
+    
+    if(contvolta == 6) {
+        clearInterval(sorteo)
+        contvolta = 0
+        numsorteado = 0
+
+
+    }
+}, 2000)
