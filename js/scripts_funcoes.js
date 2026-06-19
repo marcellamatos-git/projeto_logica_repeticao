6e2961728b82b61cc58f2833e6f13ce6304aefef
marcella <marcellamatos2428@gmail.com>
2026-06-19 14:08:53 -0300

    function

---
+++ js/scripts_funcoes.js
@@ -0,0 +1,24 @@
+function mensagem(){
+    console.log('uma mensagem')
+}
+mensagem()
+
+function mensagemlinda(txt){
+    console.log(txt)
+}
+
+mensagemlinda('Brasil vai dar um pau no brasil')
+
+function somanumeros(num1, num2){
+    return num1 + num2
+} 
+
+console.log('As somas dos numeros', somanumeros(10,58))
+
+const multiplicacao = function(num1, num2){
+    return num1 * num2
+}
+
+console.log(multiplicacao(64, 5))
+
+console.log('-------> Arrow function <---------')
